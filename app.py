07e4d805c4b2d97cc04bdea9a1f5795f976db024
Marlon Tavares <marlontf@gmail.com>
2022-07-21 18:48:25 -0300

    changes in app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import SECRET_KEY, SQLALCHEMY_DATABASE_URI
 from flask_cors import CORS
 import os.path
 
@@ -9,6 +8,7 @@ CORS(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 if os.path.exists('config.py'):
+    from config import SECRET_KEY, SQLALCHEMY_DATABASE_URI
     app.config['SECRET_KEY'] = SECRET_KEY
     app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
     app.config["JWT_SECRET_KEY"] = SECRET_KEY
