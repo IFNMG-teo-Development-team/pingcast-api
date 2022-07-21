fe015a9bcd831b2223fb440703a94e2cf24b2e96
Audrey Teles dos Santos <68817384+Audrey-Teles@users.noreply.github.com>
2022-07-21 12:23:26 -0300

    CORS e Requests do perfil
    Correção dos CORS e adição do GET de perfil por id

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask_jwt_extended import create_access_token, JWTManager,jwt_required
+from flask_jwt_extended import create_access_token, get_jwt_identity, JWTManager,jwt_required
 from config import GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, SECRET_KEY
 from flask import session, request, url_for, jsonify, json, make_response
 from flask.views import MethodView
@@ -6,6 +6,7 @@ from authlib.integrations.flask_client import OAuth
 from models import Perfil
 from app import app, db
 from flask_restful import Api, Resource
+from marshmallow import Schema
 
 jwt = JWTManager(app)
 oauth = OAuth(app)
@@ -24,43 +25,59 @@ github = oauth.register(
     client_kwargs={'scope': 'user:email'},
 )
 
-
 @app.route('/healthcheck')
 def healthcheck():
     return {"status": "OK"}
 
-# Rota para cadastro de um novo usuário, recebendo apenas requisição POST
-@app.route('/cadastrar', methods=["POST", ])
+
+# Route to login with Github
+@app.route('/login/github', methods=['POST', 'GET'])
+def github_login():
+    github = oauth.create_client('github')
+    redirect_uri = url_for('github_authorize', _external=True)
+    return github.authorize_redirect(redirect_uri)
+
+# Route to login authorization with Github
+@app.route('/login/github/authorize')
+def github_authorize():
+    token = github.authorize_access_token()
+    resp = github.get('user').json()
+    return {"token": token}
+
+# [início]\----------- Perfil -----------\
+
+#Criar novo perfil (cadastrar)
+@app.route("/perfil", methods=["POST"])
 def cadastrar():
-    # Armazena o json na variável
-    json = request.json
-    # Armazena os dados necessários nas variáveis
-    username = json['username']
-    genero = json['gender']
-    data_nascimento = json['birth_date']
-    nome = json['name']
-    email = json['email']
-    senha = json['password']
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    else:
+        # Armazena o json na variável
+        json = request.json
+        # Armazena os dados necessários nas variáveis
+        username = json['username']
+        genero = json['gender']
+        data_nascimento = json['birth_date']
+        nome = json['name']
+        email = json['email']
+        senha = json['password']
 
-    # Verifica se já existe uma conta com esse email
-    if Perfil.query.filter_by(username=username).first():
-        return {"mensagem": "Esse nome de usuário já está sendo utilizado!"}, 409
-    elif Perfil.query.filter_by(email=email).first():
-        return {"mensagem": "Esse email já está sendo utilizado!"}, 409
-        
+        # Verifica se já existe uma conta com esse email
+        if Perfil.query.filter_by(username=username).first():
+            return _corsify_actual_response(jsonify({"mensagem": "Esse nome de usuário já está sendo utilizado!"}, 409))
+        elif Perfil.query.filter_by(email=email).first():
+            return _corsify_actual_response(jsonify({"mensagem": "Esse email já está sendo utilizado!"}, 409))
+            
+        # Caso contrário continua as etapas do cadastro
+        novo_perfil = Perfil(username=username, data_nascimento=data_nascimento, genero=genero, nome=nome, email=email, senha=senha, tipo_conta='gratuita')
+        db.session.add(novo_perfil)
+        db.session.commit()
     
-    # Caso contrário continua as etapas do cadastro
-    novo_perfil = Perfil(username=username, data_nascimento=data_nascimento, genero=genero, nome=nome, email=email, senha=senha, tipo_conta='gratuita')
-    db.session.add(novo_perfil)
-    db.session.commit()
-  
-    return {"mensagem": "Cadastro realizado com sucesso!"}, 201
+        return _corsify_actual_response(jsonify({"mensagem": "Cadastro realizado com sucesso!"}, 201))
 
-
-# Rota para login
-@app.route('/login', methods=["POST", "OPTIONS"])
+# Realizar login
+@app.route("/perfil/login", methods=["POST"])
 def login():
-
     if request.method == "OPTIONS": # CORS preflight
             return _build_cors_preflight_response()
     elif request.method == "POST":
@@ -73,53 +90,31 @@ def login():
         # Consulta no banco uma ocorrência do email
         perfil = Perfil.query.filter_by(email=email, senha=senha).first()
 
-
-
         if perfil:  # Verifica se um usuário foi encontrado
-            token_acesso = create_access_token(perfil.username)
+            token_acesso = create_access_token(perfil.id)
             return _corsify_actual_response(jsonify({"token": token_acesso, "status": 200}))
 
-        return {"mensagem": "Credenciais incorretas!", "status": 204}
-    
-    else:
-        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
+        return _corsify_actual_response(jsonify({"mensagem": "Email ou senha incorretos!", "status": 204}))
 
-@app.route("/logout", methods=['POST', 'GET', ])
+# Buscar dados públicos de um perfil pelo id
+@app.route("/perfil/<id>", methods=["GET"])
 @jwt_required()
-def logout():
-    return {"mensagem": "Logout efetuado com sucesso!"}, 200
+def get_perfil(id):
+    if request.method == "OPTIONS": # CORS preflight
+            return _build_cors_preflight_response()
+    elif request.method == "GET":
+        perfil = Perfil.query.filter_by(id=id).first()
+        if perfil:
+            return _corsify_actual_response(jsonify({"perfil" : jsonConverter(PerfilSchema, perfil), "status":200}))
+        else:
+            return _corsify_actual_response(jsonify({"status": 404,
+                    "mensagem": "Perfil não encontrado!"}))
+  
+# [Final] \----------- Perfil -----------\
 
 
-# Route to login with Github
-@app.route('/login/github', methods=['POST', 'GET'])
-def github_login():
-    github = oauth.create_client('github')
-    redirect_uri = url_for('github_authorize', _external=True)
-    return github.authorize_redirect(redirect_uri)
-
-
-# Route to login authorization with Github
-@app.route('/login/github/authorize')
-def github_authorize():
-    token = github.authorize_access_token()
-    resp = github.get('user').json()
-    return {"token": token}
-
-
-# -------------------------------------------------------------
-
-class PodcastView(MethodView):
-    
-    def post(self):
-        json = request.json
-        
-        duracao = json['duracao']
-        data_postagem = json['data_postagem']
-        participantes = json['participantes']
-        nome = json['nome']
-        descricao = json['descricao']
-        
-        
+         
+# ------ Configurações dos cors ------
 def _build_cors_preflight_response():
     response = make_response()
     response.headers.add("Access-Control-Allow-Origin", "*")
@@ -130,3 +125,17 @@ def _build_cors_preflight_response():
 def _corsify_actual_response(response):
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response 
+#--------------------------------------
+
+# Classes para definição dos campos de retorno de cada tabela
+class PerfilSchema(Schema):
+    class Meta:
+        fields = ("id", "username", "data_nascimento", "interesses", "descricao", "genero", "nome", "tipo_conta")
+# ------------------------------------------------------------
+
+
+# Função que converte objeto da query em JSON
+def jsonConverter(schemaClass: object, toConvertObject: object):
+    consulta = schemaClass()
+    result = consulta.dump(toConvertObject)
+    return result
